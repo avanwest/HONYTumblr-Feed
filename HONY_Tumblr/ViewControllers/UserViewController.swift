d75eaa483623a95d030100e5e374343c0921e0a9
Adam vanWestrienen <avanwest@gmail.com>
2018-09-12 23:56:06 -0400

    upload gif

--- HONY_Tumblr/ViewControllers/UserViewController.swift
+++ HONY_Tumblr/ViewControllers/UserViewController.swift
@@ -54,9 +54,7 @@ class UserViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath as IndexPath) as! PhotoCell
-//        cell.textLabel?.text = "This is row \(indexPath.row)"
         
-
         let post = posts[indexPath.row]
 
         if let photos = post["photos"] as? [[String: Any]] {
